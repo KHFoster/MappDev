5b4c765bf81ef5b21127d8bfb021c492dfde412a
csaper6 <edgarhr.usa@gmail.com>
2017-04-27 13:51:16 -0700

    BRANCH

--- app/src/main/java/com/example/csaper6/mappdev/MainActivity.java
+++ app/src/main/java/com/example/csaper6/mappdev/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.csaper6.mappdev;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton currentOption, fab1, fab2, fab3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        currentOption = (FloatingActionButton) findViewById(R.id.fab);
+        fab1 = (FloatingActionButton) findViewById(R.id.fab_1);
+
+        currentOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation show_fab_1 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab1_show);
+                Animation hide_fab_1 = AnimationUtils.loadAnimation(getApplication(), R.anim.fab1_hide);
+
+                FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) fab1.getLayoutParams();
+                layoutParams.rightMargin += (int) (fab1.getWidth() * 1.7);
+                layoutParams.bottomMargin += (int) (fab1.getHeight() * 0.25);
+                fab1.setLayoutParams(layoutParams);
+                fab1.startAnimation(show_fab_1);
+                fab1.setClickable(true);
+            }
+        });
 
 
 
